88dd714b5e5199ee7dfb39e8df3a125a30225ec0
Shad Fernandez <shadfernandez@Shads-MacBook-Pro.local>
2021-09-24 20:29:19 -0700

    formalized naming convention for temp tables

--- scripts/homework2.sql
+++ scripts/homework2.sql
@@ -55,8 +55,8 @@ GROUP BY bc.batter;
 -- Question Number 3
 
 -- Create temp table for average batting ave of each player per day
-DROP TEMPORARY TABLE if exists batting_ave;
-CREATE TEMPORARY TABLE batting_ave
+DROP TEMPORARY TABLE if exists t_batting_ave;
+CREATE TEMPORARY TABLE t_batting_ave
 SELECT
     date(bg.local_date) as 'game_date',
     batter,
@@ -66,8 +66,8 @@ JOIN batter_counts bc on bg.game_id = bc.game_id
 GROUP BY date(bg.local_date), batter;
 
 
-DROP TEMPORARY TABLE if exists batting_ave_2;
-CREATE TEMPORARY TABLE batting_ave_2
+DROP TEMPORARY TABLE if exists t_batting_ave_2;
+CREATE TEMPORARY TABLE t_batting_ave_2
 SELECT
     date(bg.local_date) as 'game_date',
     batter,
@@ -83,9 +83,9 @@ SET SESSION cte_max_recursion_depth = 1000000;
 CREATE TEMPORARY TABLE DateRange
 WITH RECURSIVE DateRange (DateName) AS
 (
-  SELECT (select date(min(game_date)) from batting_ave)
+  SELECT (select date(min(game_date)) from t_batting_ave)
     UNION ALL
-  SELECT adddate(DateName, 1) FROM DateRange WHERE DateName < (select date(max(game_date)) from batting_ave_2)
+  SELECT adddate(DateName, 1) FROM DateRange WHERE DateName < (select date(max(game_date)) from t_batting_ave_2)
 )
 SELECT
        DateName
@@ -94,8 +94,8 @@ FROM DateRange;
 -- Cross join DateRange table to batting_ave
 -- and coalesce missing values
 -- Source: https://stackoverflow.com/questions/19075098/how-to-fill-missing-dates-by-groups-in-a-table-in-sql?rq=1
-DROP TEMPORARY TABLE if exists baseball_batting_ave;
-CREATE TEMPORARY TABLE baseball_batting_ave
+DROP TEMPORARY TABLE if exists t_baseball_batting_ave;
+CREATE TEMPORARY TABLE t_baseball_batting_ave
     SELECT
         batting_date.DateName,
         batting_date.batter,
@@ -111,21 +111,21 @@ CREATE TEMPORARY TABLE baseball_batting_ave
                    batter,
                    min(game_date) as lowest_date,
                    max(game_date) as highest_date
-            FROM batting_ave
+            FROM t_batting_ave
             GROUP BY batter ) date_param CROSS JOIN DateRange date_ranges
             WHERE date_ranges.DateName BETWEEN date_param.lowest_date and date_param.highest_date
-            ) batting_date LEFT JOIN batting_ave_2 ba2 ON batting_date.batter = ba2.batter AND batting_date.DateName = ba2.game_date
+            ) batting_date LEFT JOIN t_batting_ave_2 ba2 ON batting_date.batter = ba2.batter AND batting_date.DateName = ba2.game_date
     ORDER BY batter;
 
 -- Create a temporary table and calculate 100 day rolling average
-DROP TEMPORARY TABLE if exists baseball_rolling_ave;
-CREATE TEMPORARY TABLE baseball_rolling_ave
+DROP TEMPORARY TABLE if exists t_baseball_rolling_ave;
+CREATE TEMPORARY TABLE t_baseball_rolling_ave
     SELECT
         DateName AS game_date,
         batter,
         battingave AS batting_avg,
         round(avg(battingave) OVER (PARTITION BY batter ORDER BY DateName ASC ROWS 100 PRECEDING),3) 100_day_rollavg
-    FROM baseball_batting_ave
+    FROM t_baseball_batting_ave
     ORDER BY batter, DateName;
 
 -- Create table of 100 day rollin average for each batter
@@ -137,6 +137,9 @@ SELECT
     batter,
     batting_avg,
     100_day_rollavg
-FROM baseball_rolling_ave
+FROM t_baseball_rolling_ave
 WHERE batting_avg IS NOT NULL;
 
+select *
+from f_baseball_100_day_bat_avg
+limit 20;
