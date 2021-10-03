6b61eae8f70fb7c214dd223a03fc4aae36ebeeb7
Shad Fernandez <shadfernandez@Shads-MacBook-Pro.local>
2021-10-03 09:07:03 -0700

    connected to baseball db

--- scripts/homework3.py
+++ scripts/homework3.py
@@ -1,3 +1,18 @@
 # HW1
 # Author: Shad Fernandez
 # Date: 02-OCT-2021
+
+
+import pymysql
+
+con = pymysql.connect(host='localhost',user='root',
+                      password='sd.xd.mmc',
+                      database='baseball')
+try:
+    with con.cursor() as cur:
+        cur.execute('Show tables from baseball')
+        rows = cur.fetchall()
+        for row in rows:
+            print(f'{row[0]}')
+finally:
+    con.close()
