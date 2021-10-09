1745002f07d161c3ff5fee533f8966454d65f8aa
Shad Fernandez <shadfernandez@Shads-MacBook-Pro.local>
2021-10-08 21:51:03 -0700

    added db username and password input

--- scripts/homework3.py
+++ scripts/homework3.py
@@ -23,14 +23,16 @@ def get_spark(driver_path):
 
 def load_table_spark(spark_instance, url, driver, table_list, user, passkey):
     """
-    Retrieve table(s) from db and load to spark
+    Retrieve table(s) from db and load to spark and returns a dictionary of df
     :param table_list:
     :param spark_instance: spark instance
     :param url: url of host
     :param driver: tbe db Driver
     :param user: username
     :param passkey: passkey
+    :return dictionary containing df
     """
+    df_dict = {}
     for name in table_list:
         query_table = "(SELECT * FROM " + name + ") AS " + name
         data_frame = (
@@ -47,6 +49,8 @@ def load_table_spark(spark_instance, url, driver, table_list, user, passkey):
         )
         data_frame.persist(StorageLevel.DISK_ONLY)
         data_frame.createOrReplaceTempView(name)
+        df_dict[name] = data_frame
+    return df_dict
 
 
 def main():
@@ -54,15 +58,21 @@ def main():
     connector_file_path = "../dbConnectors/mysql-connector-java-8.0.25.jar"
     spark = get_spark(connector_file_path)
 
+    # get user input
+    print("Please enter db user info")
+    user = input("username: ")
+    passkey = input("Password: ")
     # set variables for parameters to get tables from mysql
     url = "jdbc:mysql://localhost:3306/baseball?zeroDateTimeBehavior=convertToNull"
     driver = "com.mysql.cj.jdbc.Driver"
-    user = "guest"
-    passkey = "squidgames"
     table_list = ["game", "batter_counts"]
 
     # retrieve tables and load to spark
-    load_table_spark(spark, url, driver, table_list, user, passkey)
+    table_dict = load_table_spark(spark, url, driver, table_list, user, passkey)
+
+    # show schema of selected tables
+    for obj in table_dict.values():
+        obj.printSchema()
 
     # use transformer to retrieve df with n days of batting ave rolling ave
     # set at 100 days for this examples
